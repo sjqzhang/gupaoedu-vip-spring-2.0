2487aa72f87f4e941468c77d9b5316145eee50ea
yinjianjun <yinjianjing@126.com>
2018-10-14 15:03:02 +0800

    test

---
+++ src/main/java/com/gupaoedu/vip/spring/demo/action/MyTest.java
@@ -0,0 +1,18 @@
+package com.gupaoedu.vip.spring.demo.action;
+
+import com.gupaoedu.vip.spring.formework.annotation.GPService;
+
+/**
+ * @Author: walker
+ * @Description:
+ * @Date: 2018/7/25
+ */
+
+public class MyTest {
+
+    private int a;
+
+    public void abc(){
+
+    }
+}
